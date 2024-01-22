404379c8d866d264d6f718cc2ef1f2873515f823
ahmid7 <ahmidsanni@gmail.com>
2024-01-22 14:52:05 +0100

    set up styles and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'gray-1': '#333333',
+        'mirage-black': '#101720♠'
+      },
+      fontFamily: {
+        neue: ['var(--font-neue)']
+      }
     },
   },
   plugins: [],
