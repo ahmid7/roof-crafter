3cec967c5bc42e59606618772b2e4620b3ff0ac5
ahmid7 <ahmidsanni@gmail.com>
2024-01-23 12:42:44 +0100

    Created the heroSection component

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <section className='between'>
+      <div>
+        <h2>Trusted Roofing Services for Your Home</h2>
+
+        <p>Protect your home with our reliable and professional roofing services. We offer roof repair, replacement, installation, emergency roofing, and more.</p>
+
+        <ContactForm />
+      </div>
+
+      <div>
+
+      </div>
+    </section>
+  )
+}
+
+export default HeroSectionce
