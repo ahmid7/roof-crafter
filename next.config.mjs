a5d738b8d15197458a70d4863ee204aa994a54a4
ahmid7 <ahmidsanni@gmail.com>
2024-03-12 17:08:42 +0100

    made some changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   typescript: {
     // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
+    // your project has type errors
     ignoreBuildErrors: true,
   },
 };
